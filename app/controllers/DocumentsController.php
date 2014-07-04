2bf701024a0e2a8b44d4e9f1bfea76fb4e651ebd
Dan Michael O. Heggø <danmichaelo@gmail.com>
2014-07-04 21:22:23 +0000

    Support latest simplemarcparser

--- app/controllers/DocumentsController.php
+++ app/controllers/DocumentsController.php
@@ -1,8 +1,7 @@
 <?php
 
 use Scriptotek\Sru\Client as SruClient;
-use Scriptotek\SimpleMarcParser\BibliographicParser;
-use Scriptotek\SimpleMarcParser\HoldingsParser;
+use Scriptotek\SimpleMarcParser\Parser;
 use \Guzzle\Http\Client as HttpClient;
 
 class DocumentsController extends BaseController {
@@ -34,21 +33,24 @@ class DocumentsController extends BaseController {
 
 		$data = $response->records[0]->data;
 
-		$parser = new BibliographicParser;
-		$holdingsParser = new HoldingsParser;
+		$parser = new Parser;
 		$r = $data->first('metadata/marc:collection/marc:record[@type="Bibliographic"]');
 
-		$rec = array_merge($res, $parser->parse($r));
-		$rec['sru_url'] = $sru->urlTo($query, 1, 1);
-
-		$rec['holdings'] = array();
-
-		foreach ($data->xpath('metadata/marc:collection/marc:record[@type="Holdings"]') as $holding) {
-			$h = $holdingsParser->parse($holding);
-			$rec['holdings'][] = $h;
+		$rec = $parser->parse($r);
+		$rec->source = $sru->urlTo($query, 1, 1);
+		foreach ($res as $key => $value) {
+			$rec->{$key} = $value;
 		}
 
-		$rec['subjects'] = array_map(function($subj) {
+		$holdings = array();
+
+		foreach ($data->xpath('metadata/marc:collection/marc:record[@type="Holdings"]') as $holding) {
+			$h = $parser->parse($holding);
+			$holdings[] = $h;
+		}
+		$rec->holdings = $holdings;
+
+		$rec->subjects = array_map(function($subj) {
 			$term = $subj['term'];
 			if (isset($subj['subdivisions']['topical'])) $term .= ' : ' . $subj['subdivisions']['topical'];
 			if (isset($subj['subdivisions']['form'])) $term .= ' : ' . $subj['subdivisions']['form'];
@@ -59,7 +61,7 @@ class DocumentsController extends BaseController {
 			);
 			if (isset($subj['vocabulary'])) $o['vocabulary'] = $subj['vocabulary'];
 			return $o;
-		}, $rec['subjects']);
+		}, $rec->subjects);
 
 		return Response::json($rec);
 	}
